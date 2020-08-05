401bc1cadbb26350e534ac781655dffa0b736d8a
Rodrigo Ferreira Brandão <rbrandao@farma.local>
2020-08-05 17:59:55 -0300

    Aula 2 - 1h47

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({massege: 'Hello World'})
-})
+app.use(routes);
 
 //localhost:3333/users
 app.listen(3333);
