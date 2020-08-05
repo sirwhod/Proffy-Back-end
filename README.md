87606502bd79131ddb25fd2085c03df1e075b8e7
Rodrigo Ferreira Brandão <rbrandao@farma.local>
2020-08-05 14:52:09 -0300

    Aula 2 1h25

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Proffy-Back-end
+
+## Funcionalidades
+
+### Conexões
+
+- Rota para listar o total de conexões.
+- Rota para criar uma nova conexão.
+
+### Aulas
+
+- Rota para criar uma Aula.
+- Listar aulas.
+    - Filtrar por Matéria, dia da semana e horários.
+
+## Tecnologias 
+
+- TypeScrypt
+- NodeJS
+- Express
+
+### Banco de dados
+
+- Knex
+- SQL Lite
--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,11 @@ import express from 'express';
 
 const app = express();
 
-//Rota = Endereço da aplicação
-// exemplo:
-//http://localhost:3333/users
-//http://localhost:3333/contatos
+app.use(express.json());
 
-// Recurso = é nome no final da rota.
-// nos exemplos anteriores é: "users" e "contatos"
-
-//Metodos Http
-
-//GET = Buscar ou listar alguma informação
-//POST = Criar uma nova informação
-//PUT = Alterar uma informação existente
-//DELETE = deletar uma informação existente.
-
-app.get('/users', (request, response) => {
-    const users = [
-        {name: 'rodrigo', age: '21'},
-        {name: 'felipe', age: '21'}
-    ]
-    return response.json(users)
+app.get('/', (request, response) => {
+    return response.json({massege: 'Hello World'})
 })
-
-
 
 //localhost:3333/users
 app.listen(3333);
